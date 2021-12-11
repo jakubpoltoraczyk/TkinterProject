e0ec656148d44af73c24fe21232bdbc62100faed
Filip Poltoraczyk <f.poltoraczyk@gmail.com>
2021-12-11 11:39:30 +0100

    Updated Message box class

--- messagee_box/PasswordMessageBox.py
+++ messagee_box/PasswordMessageBox.py
@@ -1,14 +1,14 @@
 import tkinter as tk
 from tkinter import messagebox
 
-class MessageBox:
+class PasswordMessageBox:
     """Class, which represents message box object"""
 
     def __init__(self, text, title):
         """Construct new message box
         Params:
-            text(str): text of message
-            title(str): title of message"""
+            text (str): text of message
+            title (str): title of message"""
 
         self.__text = text
         self.__title = title
@@ -16,4 +16,4 @@ class MessageBox:
     def show(self):
         """Method which shows message box"""
 
-        messagebox.showinfo(self.__text, self.__title)
+        messagebox.showerror(self.__text, self.__title)
