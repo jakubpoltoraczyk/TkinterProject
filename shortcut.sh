d1bd5ef25e94a636d415883ff2255c983ec70d2a
Filip Poltoraczyk <f.poltoraczyk@gmail.com>
2021-12-11 15:48:03 +0100

    shortcut updated

--- shortcut.sh
+++ shortcut.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd shortcut
-pyinstaller --onefile update_shortcut.py > /dev/null 2>&1
+pyinstaller --onefile update_shortcut.py &> /dev/null
 mv dist/update_shortcut ~/Desktop/TkinterProject
 cd ..
--- shortcut/update_shortcut.py
+++ shortcut/update_shortcut.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('python3 /home/jakub/Desktop/projekty/TkinterProject/main.py')
+os.system('python3 /home/filip/Documents/ProjectTkinter/main.py')
